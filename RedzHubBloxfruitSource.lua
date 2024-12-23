ff0653b6cb422ca189ea128bfe653a07ff936c67
ScriptingHubs <minleekim5@gmail.com>
2024-12-23 19:58:10 +0800

    Create RedzHubBloxfruitSource.lua

---
+++ RedzHubBloxfruitSource.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
+loadstring(game:HttpGet('https://darkscripts.space/scripts/f0b894be-a2ad-4236-8ef1-819786af2a48_830309842815942736.lua',true))()
